f6ff9e85ab490bdc5b45a219908584ea9bd3dbbe
MdRakibHossen0 <mdrakibhossencse@gmail.com>
2025-05-20 19:21:22 +0600

    login page done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,70 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="bg-base-100 min-h-screen">
+      <div className=" w-full max-w-md p-10 mx-auto flex items-center justify-center ">
+        <div className="card  w-full  max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body bg-base-100  ">
+            <div className="text-center ">
+              <h2 className="text-3xl font-bold text-gray-800 ">
+                Login to your Account
+              </h2>
+              <p className="text-gray-800">
+                Don't have account? Please{" "}
+                <Link
+                  className="text-blue-500 underline font-bold"
+                  to="/auth/signUp"
+                >
+                  Sign Up
+                </Link>{" "}
+                here
+              </p>
+            </div>
+
+            <button className="btn lg:w-80 bg-white text-black border border-[#e5e5e5] flex items-center gap-3">
+              <FcGoogle size={25} />
+              Login with Google
+            </button>
+            <button className="btn lg:w-80 bg-white text-black border border-[#e5e5e5] flex items-center gap-3">
+              <FaGithub size={25} />
+              Login with Google
+            </button>
+            <form className="fieldset">
+              <label className="label text-gray-600">Email</label>
+              <input
+                type="email"
+                className="input"
+                name="email"
+                placeholder="Enter Email"
+                required
+              />
+              <label className="label text-gray-600">Password</label>
+              <input
+                type="password"
+                className="input"
+                name="email"
+                placeholder="Enter your password"
+                required
+              />
+              <div>
+                <a className="link link-hover text-gray-800">
+                  Forgot password?
+                </a>
+              </div>
+
+              <button type="submit" className="btn  btn-primary   mt-4">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
